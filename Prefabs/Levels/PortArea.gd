43e3bbb17caf4a9447ad52c66a1a8e6aab7b4ee2
TheRealVP <zenmusha@gmail.com>
2022-06-23 20:54:49 +0100

    More smol changes

---
+++ Prefabs/Levels/PortArea.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_PortArea_body_entered(body):
+	if body.name=="Player":
+		$anim.play("fade out")
+		yield(get_node("anim"), "animation_finished")
+		PlayerVariables.set_lives(body.lives)
+		PlayerVariables.set_berries(body.coinNumber)
+		#if spawnPoint>0:
+		PlayerVariables.set_exit_point(0)
+		PlayerVariables.goto_scene("res://Prefabs/Levels/Level3_1.tscn" ) # Replace with function body.
