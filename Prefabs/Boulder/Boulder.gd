fb73e06bb513d6af2fa8c7a37b14ee161513e44b
mboingo <108038656+mboingo@users.noreply.github.com>
2022-06-24 16:18:27 -0300

    Finished boulder + bonus level 2, fixed player knockback

--- Prefabs/Boulder/Boulder.gd
+++ Prefabs/Boulder/Boulder.gd
@@ -1,12 +1,35 @@
 extends KinematicBody2D
 
-var speed = 80
+
+onready var animation = $anim
+
+export var speed = 120
 var velocity = Vector2.ZERO
 var direction= Vector2.RIGHT
 var gravity = 500
 
+var state = STATE.IDLE
+
+enum STATE {
+	IDLE,
+	HIT
+}
+
 
 func _process(delta):
+	match state:
+		STATE.IDLE:
+			chase(delta)
+		STATE.HIT:
+			animation.play("Hurt") #animation calls knockback_done()
+
+
+func knockback_done():
+	state = STATE.IDLE
+	animation.play("Run")
+
+
+func chase(delta):
 	velocity.x = speed * direction.x
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
@@ -16,6 +39,7 @@ func _on_hitBox_body_entered(body):
 	if body.name == "Player":
 		if body.has_method("hurt"):
 			body.hurt(0.5)
+		state = STATE.HIT
 		
 
 
