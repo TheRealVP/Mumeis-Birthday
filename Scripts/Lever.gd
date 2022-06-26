d32cc346a2355a828f097a741a42a1a662236038
mboingo <108038656+mboingo@users.noreply.github.com>
2022-06-26 19:39:54 -0300

    cheese

--- Scripts/Lever.gd
+++ Scripts/Lever.gd
@@ -2,13 +2,17 @@ extends Area2D
 
 signal is_triggered
 
+var triggered = false
+
 func _process(delta):
 	var areas = get_overlapping_areas()
 	for area in areas:
 		if area.is_in_group("Sword"):
-			$anim.play("Hit")
-			yield(get_node("anim"), "animation_finished")
-			$anim.play("Activated")
-			emit_signal("is_triggered")
+			if triggered == false:
+				$anim.play("Hit")
+				yield(get_node("anim"), "animation_finished")
+				$anim.play("Activated")
+				emit_signal("is_triggered")
+				triggered = true
 		else:
 			$anim.play("Idle")
