26bfbd57e3bcae660180d8df146c4851b6e342cc
TheRealVP <zenmusha@gmail.com>
2022-07-10 13:11:44 +0100

    Small change to level 3_7

---
+++ Prefabs/Levels/LavaPool.gd
@@ -0,0 +1,23 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+
+	var bodies = get_overlapping_bodies()
+	for body in bodies:
+		if body.name=="Player":
+			if body.invulnerable ==false:
+				body.hurt(body.lives)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
