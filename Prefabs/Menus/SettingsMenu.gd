3037bdf9b3f79608d500468f51f6595bcbaf0969
TheRealVP <zenmusha@gmail.com>
2022-07-16 19:34:47 +0100

    More playing around with sound settings

--- Prefabs/Menus/SettingsMenu.gd
+++ Prefabs/Menus/SettingsMenu.gd
@@ -1,6 +1,16 @@
 extends Control
 
 var master_bus = AudioServer.get_bus_index("Master")
+var bgm_bus = AudioServer.get_bus_index("BGM")
+var sfx_bus = AudioServer.get_bus_index("SFX")
+
+func _on_MasterSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(master_bus, value) # Replace with function body.
+
 
 func _on_BGMSlider_value_changed(value):
-	AudioServer.set_bus_volume_db(master_bus, value) # Replace with function body.
+	AudioServer.set_bus_volume_db(bgm_bus, value) # Replace with function body.
+
+
+func _on_SFXSlider_value_changed(value):
+	AudioServer.set_bus_volume_db(sfx_bus, value) # Replace with function body.
